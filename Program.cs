a687453f2d264bac33ac23a206740ee3dc5969b1
subodhNagalwade <subodhnagalwade@gmail.com>
2022-10-16 12:44:08 +0530

    [SUBODH]Employee payroll ADO Retrive a data

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace EmployeePayRollADO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wel-Come to Employee payroll");
+
+            EmployeeModel model = new EmployeeModel();
+            EmployeeRepo repo = new EmployeeRepo();
+
+            model.EmployeeName = "Bruce";
+            model.PhoneNumber = 1234567890;
+            model.Address = "Banglore";
+            model.Department = "HR";
+            model.Gender = 'M';
+            model.BasicPay = 22000;
+            model.Deduction = 1500;
+            model.TaxablePay = 200;
+            model.Tax = 300;
+            model.NetPay = 2500;
+            model.City = "Benglore";
+            model.Country = "India";
+
+            //repo.AddEmployee(model);
+           // repo.GetAllEmployee();
+           // DataBaseQuerry data = new DataBaseQuerry();
+           
+           // data.SumFunction();
+           // data.AvgFunction();
+           // data.MinFunction();
+           // data.MaxFunction();
+            //data.CountFunction();
+        }
+    }
+}
